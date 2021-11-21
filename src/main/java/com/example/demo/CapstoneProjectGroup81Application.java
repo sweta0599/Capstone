d5903e41af0a0eb079440be7fc91d4ba0ade37c6
Sweta <banerjeesweta0599@gmail.com>
2021-11-21 20:15:39 +0530

    commit

---
+++ src/main/java/com/example/demo/CapstoneProjectGroup81Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectGroup81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectGroup81Application.class, args);
+	}
+
+}
